ed6ee56a9630c4fca24bfe3c4d15e020bd3dd99c
kanr1 <kanr1@LAPTOP-KK9SFS6B>
2019-09-18 23:07:09 +0530

    Committing my project

---
+++ Newproj1/src/com/Newproj1/firstproj.java
@@ -0,0 +1,12 @@
+package com.Newproj1;
+
+public class firstproj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
